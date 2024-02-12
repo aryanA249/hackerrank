1816d38bb8db1afd430d4a81d41436268033e8d8
Aryan Sharma <146845209+aryanA249@users.noreply.github.com>
2024-02-12 21:42:53 +0530

    Create hackerrank1.c

---
+++ hackerrank1.c
@@ -0,0 +1,62 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+
+
+int main()
+{
+  int x;
+  
+  scanf("%d",&x);
+  if(x==1)
+  {
+     printf("one"); 
+  }
+  else if(x==2)
+  {
+      printf("two");
+  }
+  else if(x==3)
+  {
+      printf("three");
+  }
+  else if(x==4)
+  {
+      printf("four");
+  }
+  else if(x==5)
+  {
+      printf("five");
+      
+  }
+  else if(x==6)
+  {
+      printf("six");
+  }
+  else if(x==7)
+  {
+      printf("seven");
+  }
+  else if(x==8)
+  {
+      printf("eight");
+  }
+  else if(x==9)
+  {
+      printf("nine");
+  }
+  else if(x>9)
+  {
+      printf("Greater than 9");
+  }
+  return 0;
+}
